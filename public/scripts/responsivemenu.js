8b31fc15c447f251a28376988222eb4bbe57bf86
mastakillahBlitzar <juano.diy@gmail.com>
2017-09-26 23:52:42 -0500

    responsive slider added

--- public/scripts/responsivemenu.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    "use strict";
-    var x = document.getElementById("myMenu");
-    if (x.className === "menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "menu";
-    }
-}
